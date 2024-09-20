c41f8ee9cc76ca8eb7ce636ce89050df06c7a777
yangwoohyeon <dngus0717@gmail.com>
2024-09-20 13:23:20 +0900

    지역기반 무장애 필터링 구현 완료

--- src/main/java/app/app/TouristApi/Service/TouristSpotService.java
+++ src/main/java/app/app/TouristApi/Service/TouristSpotService.java
@@ -51,15 +51,15 @@ public class TouristSpotService {
             List<String> sigunguCodes = AreaMapper.getSigunguCodesForMetropolitan(areaCode);
             touristSpots = touristInfoRepository.findByAreaCodeAndSigunguCodeIn(areaCode, sigunguCodes);
         } else {
-            // "순천시"와 같은 비광역시의 경우
+            //비광역시의 경우
             areaCode = AreaMapper.getAreaCodeByRegionName(region);
             if (areaCode == null) {
                 throw new IllegalArgumentException("Invalid region: " + region);
             }
             logger.debug("Non-metropolitan Area - Region: " + region + ", AreaCode: " + areaCode);
 
-            // "순천시"에 해당하는 시군구 코드만 검색
-            List<String> sigunguCodes = AreaMapper.getSigunguCodesForNonMetropolitan("순천시");
+
+            List<String> sigunguCodes = AreaMapper.getSigunguCodesForNonMetropolitan(region);
             touristSpots = touristInfoRepository.findByAreaCodeAndSigunguCodeIn(areaCode, sigunguCodes);
 
             return filterByAccessibleFeature(touristSpots, accessibleFeature);
